62fb0a7b672f50a57984e1ebf8180f8eaf8b18f1
Saul Rivera <saul.rivera_mora@outlook.com>
2023-04-25 23:23:11 -0500

    Actividad2

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <div class="jokes">
+    <h1>Chuck Norris Jokes</h1>
+    <ul>
+      <li v-for="joke in jokes" :key="joke.value">{{ joke.value }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      jokes: [
+        {
+          "value": "Chuck Norris can skydive into outer space."
+        },
+        {
+          "value": "The chief export of Chuck Norris is pain."
+        },
+        {
+          "value": "Chuck Norris doesn't read books. He stares them down until he gets the information he wants."
+        },
+        {
+          "value": "Time waits for no man. Unless that man is Chuck Norris."
+        },
+        {
+          "value": "If you spell Chuck Norris in Scrabble, you win. Forever."
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.jokes {
+  font-family: Arial, sans-serif;
+  margin: 0 auto;
+  width: 60%;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin-bottom: 10px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+</style>
+
